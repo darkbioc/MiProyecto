ff171e45f5b4e2eaea0eb62bf8b982e55c89cfac
Bruno <brunotazu@gmail.com>
2019-01-07 20:41:00 +0100

    SignUp done

--- app/src/main/java/ml/darkbioc/miproyecto/SignUpActivity.java
+++ app/src/main/java/ml/darkbioc/miproyecto/SignUpActivity.java
@@ -1,16 +1,28 @@
 package ml.darkbioc.miproyecto;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity
 {
 
+	private static final String TAG= "MyDebug";
 	Button goBack;
 	Button signUp;
+
+	private FirebaseAuth mAuth;
 
 	EditText etPass;
 	EditText etRepPass;
@@ -21,6 +33,8 @@ public class SignUpActivity extends AppCompatActivity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_up);
+
+		mAuth = FirebaseAuth.getInstance();
 
 		goBack = findViewById(R.id.bBackSignUp);
 		signUp = findViewById(R.id.bSignUp);
@@ -42,7 +56,44 @@ public class SignUpActivity extends AppCompatActivity
 		{
 			@Override public void onClick(View v)
 			{
-				//if( )
+				String password = etPass.getText().toString();
+				String email = etMail.getText().toString();
+
+				if(etUserName.getText().toString().length() == 0 || password.length() == 0 || etRepPass.getText().toString().length() == 0 || email.length() == 0)
+				{
+					Toast.makeText(getApplicationContext(),R.string.ToastVoidFields,Toast.LENGTH_LONG).show();
+				}
+				else if(etUserName.getText().toString().length() < 4)
+				{
+					Toast.makeText(getApplicationContext(),R.string.ToastUserShort,Toast.LENGTH_LONG).show();
+				}
+				else if(!password.equals(etRepPass.getText().toString()))
+				{
+					Toast.makeText(getApplicationContext(),R.string.ToastRePassFail,Toast.LENGTH_LONG).show();
+				}
+				else
+				{
+					mAuth.createUserWithEmailAndPassword(email, password)
+							.addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+								@Override
+								public void onComplete(@NonNull Task<AuthResult> task) {
+									if (task.isSuccessful()) {
+										// Sign in success, update UI with the signed-in user's information
+										Log.d(TAG, "createUserWithEmail:success");
+										FirebaseUser user = mAuth.getCurrentUser();
+										finish();
+										//updateUI(user);
+									} else {
+										// If sign in fails, display a message to the user.
+										Log.w(TAG, "createUserWithEmail:failure", task.getException());
+										Toast.makeText(getApplicationContext(), task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+										//updateUI(null);
+									}
+
+									// ...
+								}
+							});
+				}
 			}
 		});
 	}
